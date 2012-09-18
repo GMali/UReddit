0f27a2f5a2234edb8f7d99481f1e41a7cbda292a
Gaurav Mali <hello@gauravmali.com>
2012-09-17 20:28:27 -0400

    No idea why this didnt work the first time. Use done method in mocha.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 test:
-	@./node_modules/.bin/mocha -u bdd --reporter landing --require should --timeout 1s
+	@./node_modules/.bin/mocha -u bdd --reporter landing --require should --timeout 20s
 
 .PHONY: test
--- test/test.trial.js
+++ test/test.trial.js
@@ -7,37 +7,37 @@ describe('UReddit', function() {
 	it('should have a catalog', function(done) {
 		uni.catalog(function(data){
 			should.exist(data);
-			// done();
+			done();
 		});
 	});
 	it('should have a category', function(done) {
 		uni.category("23442", function(data){
 			should.exist(data);
-			// done();
+			done();
 		});
 	});
 	it('should have a class', function(done) {
 		uni.class("23466", function(data){
 			should.exist(data);
-			// done();
+			done();
 		});
 	});
 	it('should have a lecture', function(done) {
 		uni.lecture("23464", function(data){
 			should.exist(data);
-			// done();
+			done();
 		});
 	});
 	it('should have a link', function(done) {
 		uni.link("23464", function(data){
 			should.exist(data);
-			// done();
+			done();
 		});
 	});
 	it('should have a user', function(done) {
 		uni.user("8", function(data){
 			should.exist(data);
-			// done();
+			done();
 		});
 	});
 });
